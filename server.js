966f023a59cf057d26348fc576ab18e40fbb27de
MoisesPH <moisess.pleytez@gmail.com>
2026-09-16 07:41:18 -0600

    dfafdafa

--- server.js
+++ server.js
@@ -2,24 +2,24 @@
 npm start
 */
 
-const express = require('express');
-
-const mongodb = require('./data/database');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./data/database");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'));
+app.use(bodyParser.json());
+app.use("/", require("./routes"));
 
-
-mongodb.initDb((err => {
-    if (err){
-        console.log(err);
-    }
-    else{
-app.listen(port, () => {console.log(`Database is listening and node Running on port ${port} \nhttp://localhost:${port}/\nThe second web is http://localhost:${port}/contacts`);
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(
+        `Database is listening and node Running on port ${port} \nhttp://localhost:${port}/\nThe second web is http://localhost:${port}/contacts`,
+      );
+    });
+  }
 });
-    }
-}))
-
-
